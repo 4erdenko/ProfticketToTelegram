57d60ba30b0a9a0c8d09e63f1133139ccc083692
4erdenko <4erdenko@tuta.io>
2023-10-19 21:05:34 +0300

    async parser

--- json_library/profticket_place_parser.py
+++ json_library/profticket_place_parser.py
@@ -1,46 +1,45 @@
+import asyncio
 import json
-import time
-
+from tenacity import retry, stop_after_attempt, wait_fixed
 import httpx
 
-# Define the range of IDs to fetch data for
-id_range = range(1, 700)
 
-# Initialize an empty dictionary to store the fetched data
-places_dump = {}
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(10))
+async def fetch_place(client, place_id):
+    url = f'https://widget.profticket.ru/api/company/data/?id={place_id}&language=ru-RU'
+    page = await client.get(url)
+    return page.json()
 
-# Iterate through the IDs and fetch data for each ID
-for place_id in id_range:
-    # Construct the URL for the API request
-    url = (
-        f'https://widget.profticket.ru/'
-        f'api/company/data/?id={place_id}&language=ru-RU'
-    )
 
-    # Try to fetch the data from the API
-    try:
-        # Send the HTTP GET request to the API
-        page = httpx.get(url)
+async def parser():
+    # Define the range of IDs to fetch data for
+    id_range = range(1, 700)
 
-        # Parse the JSON response
-        file = json.loads(page.text)
+    # Initialize an empty dictionary to store the fetched data
+    places_dump = {}
+    async with httpx.AsyncClient() as client:
+        # Iterate through the IDs and fetch data for each ID
+        for place_id in id_range:
+            try:
+                file = await fetch_place(client, place_id)
 
-        # Check if the response contains the required information
-        if 'response' in file:
-            # Extract the place name
-            name = file['response']['name']
+                # Check if the response contains the required information
+                if 'response' in file:
+                    # Extract the place name
+                    name = file['response']['name']
 
-            # Add the place name to the dictionary
-            places_dump[place_id] = name
-            print(place_id, name)
+                    # Add the place name to the dictionary
+                    places_dump[place_id] = name
+                    print(place_id, name)
 
-            # Save the dictionary to a JSON file
-            with open('profticket_test.json', 'w', encoding='utf-8') as f:
-                json.dump(places_dump, f, ensure_ascii=False, indent=4)
+                    # Save the dictionary to a JSON file
+                    with open(
+                        'profticket_test.json', 'w', encoding='utf-8'
+                    ) as f:
+                        json.dump(places_dump, f, ensure_ascii=False, indent=4)
+            except Exception as e:
+                print(f'Ошибка при обработке ID {place_id}: {e}')
 
-    except Exception as e:
-        # If an exception occurs, print the error message and wait for
-        # 3 seconds before continuing
-        print(e)
-        time.sleep(3)
-        continue
+
+if __name__ == '__main__':
+    asyncio.run(parser())
