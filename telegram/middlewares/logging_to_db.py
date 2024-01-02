b75c6b13c5a5767537748239e2c21607f3d2b4e1
4erdenko <4erdenko@tuta.io>
2024-01-02 23:00:00 +0300

    Add middlewares for session management, logging, and throttling
    This commit adds new middlewares to the Telegram project. It includes a DbSessionMiddleware for managing database sessions, a UserLoggingMiddleware for logging user interactions, and a ThrottlingMiddleware to prevent spamming. These enhancements aim to improve the performance, security, and user experience of the bot.

---
+++ telegram/middlewares/logging_to_db.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware, types
+from aiogram.types import TelegramObject, Update
+
+from telegram.db.models import User
+
+logger = logging.getLogger(__name__)
+
+
+class UserLoggingMiddleware(BaseMiddleware):
+    def __init__(self):
+
+        super().__init__()
+
+    async def on_process_message(
+        self, update: types.Update, data: Dict[str, Any]
+    ) -> None:
+        session = data['session']
+        bot = update.bot
+        if update.message:
+            message = update.message
+            user = await session.get(User, message.from_user.id)
+            if not user:
+                user = User(
+                    user_id=message.from_user.id,
+                    username=message.from_user.username,
+                    full_name=message.from_user.full_name,
+                )
+                session.add(user)
+                await session.commit()
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: Update,
+        data: Dict[str, Any],
+    ) -> Any:
+        if isinstance(event, Update):
+            await self.on_process_message(event, data)
+        result = await handler(event, data)
+        return result
---
+++ telegram/middlewares/throttling.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from aiogram import BaseMiddleware
+from cachetools import TTLCache
+
+from telegram.db import User
+from config import settings
+
+if TYPE_CHECKING:
+    from collections.abc import Awaitable, Callable
+    from typing import Any
+
+    from aiogram.types import TelegramObject
+
+
+class ThrottledError(Exception):
+    pass
+
+
+@dataclass(kw_only=True, slots=True)
+class ThrottlingData:
+    rate: int = 0
+    sent_warning: bool = False
+
+
+class ThrottlingMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self._cache: TTLCache[int, ThrottlingData] = TTLCache(
+            maxsize=10_000, ttl=settings.TTL_IN_SEC
+        )
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any | None:
+
+        event_user: User = data['event_from_user']
+
+        session = data['session']
+        if not event_user:
+            return None
+
+        if event_user.id not in self._cache:
+
+            self._cache[event_user.id] = ThrottlingData()
+
+        throttling_data = self._cache[event_user.id]
+
+        if throttling_data.rate == settings.MAX_RATE_SEC_IN_TTL:
+            self._cache[event_user.id] = throttling_data
+
+            if not throttling_data.sent_warning:
+                throttling_data.sent_warning = True
+                user = await session.get(User, event_user.id)
+                user.throttling += 1
+                await session.commit()
+                raise ThrottledError
+
+            return None
+        throttling_data.rate += 1
+
+        return await handler(event, data)
