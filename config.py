3830b9c8012b6e20f220f04d766c364484ad47c5
4erdenko <4erdenko@tuta.io>
2024-01-02 23:00:42 +0300

    Create new config.py file
    This commit introduces a new configuration file, config.py. The file includes classes and variables for basic system settings such as BOT, Throttling, DB, and channels. It also handles environment file loading if not running in a Docker container.

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # Bot
+    BOT_TOKEN: str
+    TEST_BOT_TOKEN: str
+    ADMIN_ID: int
+    ADMIN_USERNAME: str
+    MAINTENANCE: bool
+    #
+    # # Throttling
+    # TTL_IN_SEC: int = 20
+    # MAX_RATE_SEC_IN_TTL: int = 5
+    # # DB
+    DB_URL: str
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    PGADMIN_DEFAULT_PASSWORD: str
+    PGADMIN_DEFAULT_EMAIL: str
+    # # Channel
+    # CHANNEL_ID: str
+
+    class Config:
+        if os.environ.get('IN_DOCKER') != '1':
+            env_file = '.env'
+            env_file_encoding = 'utf-8'
+
+
+settings = Settings()
