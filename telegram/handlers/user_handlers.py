cdb75423865514bc6fb566e60e0bd2d99fd6696f
4erdenko <4erdenko@tuta.io>
2024-01-02 23:18:41 +0300

    Refactor user_handlers in telegram module
    Updated the `cmd_start` function to support the main_keyboard and added a new function to handle 'Этот месяц' command in the `user_handlers.py

--- telegram/handlers/user_handlers.py
+++ telegram/handlers/user_handlers.py
@@ -3,42 +3,48 @@ import logging
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from telegram.keyboards.main_keyboard import main_keyboard
 
 user_router = Router(name=__name__)
 logger = logging.getLogger(__name__)
 
 
 @user_router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Hi!')
-    pass
+async def cmd_start(message: Message, session: AsyncSession):
+    await message.answer(
+        'Hi!', reply_markup=await main_keyboard(message, session)
+    )
 
-# @user_router.message(F.text == 'Этот месяц')
-# async def this_month_command(message: Message):
-#     """
-#     Handles the 'Этот месяц' command.
-#
-#     Returns:
-#         Sends a message with this month's performance data.
-#     """
-#     try:
-#         msg = await message.answer(WAIT_MSG)
-#         await bot.send_chat_action(
-#             message.chat.id,
-#             'typing',
-#         )
-#         await send_chunks_edit(message.chat.id, msg, pt.get_special_info())
-#
-#         logger.info(
-#             f'{message.from_user.full_name} '
-#             f'(@{message.from_user.username}) '
-#             f'ID({message.from_user.id}) got shows for this month'
-#         )
-#     except Exception as e:
-#         await message.answer(
-#             'Произошла ошибка, попробуйте ещё раз через минутку.'
-#         )
-#         logger.error(e)
+
+
+@user_router.message(F.text == 'Этот месяц')
+async def this_month_command(message: Message):
+    """
+    Handles the 'Этот месяц' command.
+
+    Returns:
+        Sends a message with this month's performance data.
+    """
+    try:
+        msg = await message.answer(WAIT_MSG)
+        await bot.send_chat_action(
+            message.chat.id,
+            'typing',
+        )
+        await send_chunks_edit(message.chat.id, msg, pt.get_special_info())
+
+        logger.info(
+            f'{message.from_user.full_name} '
+            f'(@{message.from_user.username}) '
+            f'ID({message.from_user.id}) got shows for this month'
+        )
+    except Exception as e:
+        await message.answer(
+            'Произошла ошибка, попробуйте ещё раз через минутку.'
+        )
+        logger.error(e)
 #
 #
 # @user_router.message(F.text == 'Следующий месяц')
