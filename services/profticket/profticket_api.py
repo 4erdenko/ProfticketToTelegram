5db1d1104be1e2d23127e1825b1327871a656e04
4erdenko <4erdenko@tuta.io>
2024-01-02 23:50:50 +0300

    Refactor and improve async handling in profticket API
    The changes introduce asynchronous data loading, error handling, and improve the general flow of the 'ProfticketsInfo' class in the 'profticket_api.py' file. Legacy synchronous methods were replaced with async requests, and unnecessary methods were removed, improving efficiency and readability. A 'main' function for standalone testing has also been added.

--- services/profticket/profticket_api.py
+++ services/profticket/profticket_api.py
@@ -1,140 +1,121 @@
+
 import httpx
-
-from settings.config import (COM_ID, MAX_MSG_LEN, P_SHOWS, get_current_month,
-                             get_current_year, pluralize)
-
-result_dict = {}
-
+import asyncio
 
 class ProfticketsInfo:
-    """
-    Class for information about events from spa.profticket.ru
-    """
+    BASE_URL = 'https://widget.profticket.ru/api/event/list/?company_id='
+    EVENT_DATA_URL = 'https://widget.profticket.ru/widget-api/events-data/'
+    CUSTOMER_BUY_URL = 'https://spa.profticket.ru/customer/'
 
-    def __init__(self, com_id=None, page_num=None, month=None, year=None):
-        if month is None:
-            month = get_current_month()
-        if year is None:
-            year = get_current_year()
-        if page_num is None:
-            page_num = 1
+    def __init__(self, com_id):
         if com_id is None:
             raise ValueError('Не указан ID компании')
         self.com_id = com_id
-        self.page_num = page_num
+        self.month = None
+        self.year = None
+        self.client = httpx.AsyncClient()
+
+    def set_date(self, month, year):
         self.month = month
         self.year = year
-        self.event_id = ''
-        self.show_id = ''
-        self.main_url = (
-            f'https://widget.profticket.ru/api/event/list'
-            f'/?company_id={com_id}&type=events&page='
-            f'{page_num}&period_id=4&hall_id=&date={year}.{month}'
+
+    def _create_url(self, page_num):
+        return (
+            f'{self.BASE_URL}{self.com_id}'
+            f'&type=events&page={page_num}&period_id=4&hall_id=&date='
+            f'{self.year}.{self.month}'
             f'&name=&language=ru-RU'
         )
-        self.page = httpx.get(self.main_url)
-        self.main_json = self.page.json()
-        if 'response' not in self.main_json:
-            raise ValueError('Проблемы с получением словаря с сервера!')
-        self.page_count = self.main_json.get('response').get('page_count')
-        self.items = self.main_json.get('response').get('items')
-        if self.page_count > 1:
-            for page_num in range(2, self.page_count + 1):
-                self.main_url = (
-                    f'https://widget.profticket.ru/api/event/list'
-                    f'/?company_id={com_id}&type=events'
-                    f'&page={page_num}&period_id=4&hall_id='
-                    f'&date={year}.{month}'
-                    f'&name=&language=ru-RU'
-                )
-                self.page = httpx.get(self.main_url)
-                self.main_json = self.page.json()
-                self.items += self.main_json.get('response').get('items')
 
-    def _places(self):
-        """
-        Retrieves a dictionary with the number of available
-        seats for each event.
+    async def _load_data(self):
+        page_num = 1
+        items = []
 
-        Returns:
-            dict: A dictionary containing event IDs as keys and the number
-            of available seats as values.
-        """
-        self.places_url = (
-            f'https://widget.profticket.ru/widget-api/'
-            f'events-data/{self.com_id}/'
-        )
-        self.places_page = httpx.get(self.places_url)
-        self.places_ben = self.places_page.json()
-        self.places_events = self.places_ben.get('events')
-        self.free_places = {}
-        self.event_ids = {}
-        for event_id, free_places in self.places_events.items():
-            seats = free_places['seats']
-            self.free_places[event_id] = seats
-            self.event_ids[event_id] = event_id
+        while True:
+            url = self._create_url(page_num)
+            response = await self.client.get(url)
+            response_json = response.json()
+            if 'response' not in response_json:
+                raise ValueError('Проблемы с получением данных с сервера')
 
-    def _get_buy_link(self):
-        """
-        Generates a ticket purchase link for each event.
+            new_items = response_json.get('response').get('items')
+            if not new_items:
+                break
 
-        Returns:
-            None: The method modifies the 'buy_link' attribute for each event
-            in the 'items' attribute of the object.
-        """
-        for get_info in self.items:
+            items.extend(new_items)
+            page_num += 1
+
+        return items
+
+    async def _places(self):
+        places_url = f'{self.EVENT_DATA_URL}{self.com_id}/'
+        response = await self.client.get(places_url)
+        places_ben = response.json()
+        places_events = places_ben.get('events')
+        self.free_places = {
+            event_id: free_places['seats']
+            for event_id, free_places in places_events.items()
+        }
+
+    async def _get_buy_link(self, items):
+        for get_info in items:
             for event in get_info.get('events'):
-                self.event_id = event.get('id')
-                self.show_id = event.get('show').get('show_id')
-                self.buy_link = (
-                    f'https://spa.profticket.ru/customer/'
-                    f'{self.com_id}/shows/'
-                    f'{self.show_id}?eventsIds%5B%5D='
-                    f'{self.event_id}&language=ru-RU'
+                event_id = event.get('id')
+                show_id = event.get('show').get('show_id')
+                buy_link = (
+                    f'{self.CUSTOMER_BUY_URL}{self.com_id}/shows/{show_id}'
+                    f'?eventsIds%5B%5D={event_id}&language=ru-RU'
                 )
-                event['buy_link'] = self.buy_link
+                event['buy_link'] = buy_link
 
-    def get_full_info(self):
-        """
-        Retrieves a complete dictionary with performance information.
-
-        Returns:
-            dict: A dictionary containing all performance details
-            such as event ID, theater, scene, show name, date, duration,
-            age restriction, available seats, image URL, annotation,
-            price range, Pushkin card availability, and buying link.
-
-        Raises:
-            ValueError: If there is a problem obtaining the dictionary
-            with available seats.
-        """
+    async def get_full_info(self):
         try:
-            self._places()
+            items = await self._load_data()
+            await self._places()
+            await self._get_buy_link(items)
+
+            result = {}
+            for get_info in items:
+                for event in get_info.get('events'):
+                    result[event.get('id')] = {
+                        'id': event.get('id'),
+                        'show_id': event.get('show').get('show_id'),
+                        'theater': event.get('location_name'),
+                        'scene': event.get('location_scene'),
+                        'show_name': event.get('show_name'),
+                        'date': event.get('date_formatted'),
+                        'duration': event.get('show').get('duration'),
+                        'age': event.get('show').get('age'),
+                        'seats': self.free_places.get(event.get('id')),
+                        'image': event.get('show').get('image_url'),
+                        'annotation': event.get('annotation'),
+                        'min_price': event.get('min_price'),
+                        'max_price': event.get('max_price'),
+                        'pushkin': event.get('pushkin_card').get('can_buy'),
+                        'buy_link': event.get('buy_link'),
+                    }
+            return result
         except Exception as e:
-            raise ValueError(f'Проблемы с получением словаря с местами {e}')
-        result = {}
-        self._get_buy_link()
-        for get_info in self.items:
-            for event in get_info.get('events'):
-                my_dict = {
-                    'id': event.get('id'),
-                    'show_id': event.get('show').get('show_id'),
-                    'theater': event.get('location_name'),
-                    'scene': event.get('location_scene'),
-                    'show_name': event.get('show_name'),
-                    'date': event.get('date_formatted'),
-                    'duration': event.get('show').get('duration'),
-                    'age': event.get('show').get('age'),
-                    'seats': self.free_places.get(event.get('id')),
-                    'image': event.get('show').get('image_url'),
-                    'annotation': event.get('annotation'),
-                    'min_price': event.get('min_price'),
-                    'max_price': event.get('max_price'),
-                    'pushkin': event.get('pushkin_card').get('can_buy'),
-                    'buy_link': event.get('buy_link'),
-                }
+            raise SystemError(f'Ошибка при получении полной информации: {e}')
 
-                result[event.get('id')] = my_dict
-        return result
+    async def close(self):
+        await self.client.aclose()
 
 
+async def main():
+    from pprint import pprint
+
+    com_id = 30
+    proftickets_info = ProfticketsInfo(com_id)
+
+    proftickets_info.set_date('01', '2024')
+    try:
+        full_info = await proftickets_info.get_full_info()
+        pprint(full_info)
+    except Exception as e:
+        print(f'Ошибка: {e}')
+    finally:
+        await proftickets_info.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
