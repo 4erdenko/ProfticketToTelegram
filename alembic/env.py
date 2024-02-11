87a3e386b801a4e707122adb920932b88616342e
4erdenko <4erdenko@tuta.io>
2024-02-11 19:57:49 +0300

    Update import order in alembic/env.py
    The import order in alembic/env.py file has been corrected to follow the conventions. The "from telegram.db import Base" line of code was moved down to follow the "from config import settings" import.

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from telegram.db import Base
 from config import settings
+from telegram.db import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
