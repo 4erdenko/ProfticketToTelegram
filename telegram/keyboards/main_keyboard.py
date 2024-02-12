bc6eee6be3e5de276566e2ce808e045e16361722
4erdenko <4erdenko@tuta.io>
2024-02-13 00:59:00 +0300

    Update user-specific keyboard in main_keyboard.py
    Modified the keyboard layout in the `main_keyboard` function to be more user-specific. Depending on whether the user has any spectacles saved, the keyboard layout will display an option to view their spectacles or to set spectacles. The user's spectacles' title is also now used in the label of the button for viewing their spectacles.

--- telegram/keyboards/main_keyboard.py
+++ telegram/keyboards/main_keyboard.py
@@ -2,32 +2,37 @@ from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from telegram.db.user_operations import get_user
+from telegram.lexicon.lexicon_ru import LEXICON_BUTTONS_RU
 
 
 async def main_keyboard(message: Message, session: AsyncSession):
     user_id = message.from_user.id
     user = await get_user(session, user_id)
 
-    personal_menu_keyboard = ReplyKeyboardMarkup(
-        keyboard=[
-            [
-                KeyboardButton(text='Этот месяц'),
-                KeyboardButton(text='Следующий месяц'),
+    if user.spectacle_full_name:
+        spectacle_full_name = user.spectacle_full_name.title()
+        button = LEXICON_BUTTONS_RU['/shows_with'] + spectacle_full_name
+        personal_menu_keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text='Этот месяц'),
+                    KeyboardButton(text='Следующий месяц'),
+                ],
+                [KeyboardButton(text=button)],
             ],
-            [KeyboardButton(text='Мои спектакли')],
-        ],
-        resize_keyboard=True,
-        is_persistent=True,
-    )
+            resize_keyboard=True,
+            is_persistent=True,
+        )
+        return personal_menu_keyboard
 
     menu_keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text='Этот месяц'),
              KeyboardButton(text='Следующий месяц')],
+            [KeyboardButton(text=LEXICON_BUTTONS_RU['/set_fighter'])],
         ],
         resize_keyboard=True,
         is_persistent=True,
     )
-    if user.spectacle_full_name:
-        return personal_menu_keyboard
+
     return menu_keyboard
