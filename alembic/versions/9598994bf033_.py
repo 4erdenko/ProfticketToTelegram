f4fb86453eda372c8560a08fe9c324e7f78cd66e
4erdenko <4erdenko@tuta.io>
2025-02-04 14:18:08 +0300

    Refactor and expand Russian lexicon structure.
    Reorganized lexicon into categorized dictionaries for improved readability and maintainability. Added translations for months and new log messages for enhanced system and user interactions. Minor import adjustments in Alembic migration script.

--- alembic/versions/9598994bf033_.py
+++ alembic/versions/9598994bf033_.py
@@ -7,9 +7,9 @@ Create Date: 2024-12-29 21:41:30.296496
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '9598994bf033'
