a32fead6d5c8f0b582be6fd38b9ef81dff76bacf
4erdenko <93295097+4erdenko@users.noreply.github.com>
2025-05-17 23:25:10 +0300

    Develop (#2)
    * Add unit tests for utility functions
    
    * Add pytest to requirements.
    
    Included pytest in requirements.txt to facilitate testing and ensure consistency in the development environment. This change supports improved testing workflows and reliability.
    
    * Update requirements.txt
    
    * Update requirements.txt

---
+++ tests/__init__.py
@@ -0,0 +1,95 @@
+import sys
+import types
+
+modules = {}
+
+# config stub
+config = types.ModuleType('config')
+class Settings:
+    COM_ID = '1'
+    MAX_MSG_LEN = 100
+    ADMIN_ID = 1
+    ADMIN_USERNAME = 'admin'
+    PROXY_URL = ''
+    STOP_AFTER_ATTEMPT = 3
+    DEFAULT_TIMEZONE = 'Europe/Moscow'
+config.settings = Settings()
+modules['config'] = config
+
+# pymorphy2 stub
+pymorphy2 = types.ModuleType('pymorphy2')
+class MorphAnalyzer:
+    def parse(self, word):
+        class Res:
+            def __init__(self, w):
+                self.w = w
+            def make_agree_with_number(self, num):
+                class Word:
+                    def __init__(self, txt):
+                        self.word = txt
+                return Word(f"{self.w}_{num}")
+        return [Res(word)]
+pymorphy2.MorphAnalyzer = MorphAnalyzer
+modules['pymorphy2'] = pymorphy2
+
+# httpx stub
+httpx = types.ModuleType('httpx')
+class AsyncClient:
+    def __init__(self, *a, **kw):
+        pass
+class Response:
+    pass
+class Limits:
+    def __init__(self, *a, **kw):
+        pass
+httpx.AsyncClient = AsyncClient
+httpx.Response = Response
+httpx.Limits = Limits
+httpx.TimeoutException = Exception
+httpx.ProxyError = Exception
+httpx.HTTPStatusError = Exception
+modules['httpx'] = httpx
+
+# fake_useragent stub
+fake_useragent = types.ModuleType('fake_useragent')
+class UserAgent:
+    @property
+    def random(self):
+        return 'agent'
+fake_useragent.UserAgent = UserAgent
+modules['fake_useragent'] = fake_useragent
+
+# tenacity stub
+tenacity = types.ModuleType('tenacity')
+tenacity.retry = lambda *a, **k: (lambda f: f)
+tenacity.retry_if_exception_type = lambda *a, **k: None
+tenacity.stop_after_attempt = lambda *a, **k: None
+tenacity.wait_exponential = lambda *a, **k: None
+modules['tenacity'] = tenacity
+
+# pytz stub
+pytz = types.ModuleType('pytz')
+pytz.timezone = lambda tz: None
+modules['pytz'] = pytz
+
+# dateutil stub
+dateutil = types.ModuleType('dateutil')
+rdelta = types.ModuleType('dateutil.relativedelta')
+class relativedelta:
+    def __init__(self, *a, **k):
+        pass
+rdelta.relativedelta = relativedelta
+modules['dateutil'] = dateutil
+modules['dateutil.relativedelta'] = rdelta
+
+# aiogram stub
+aiogram = types.ModuleType('aiogram')
+aiogram_types = types.ModuleType('aiogram.types')
+class Message:
+    def __init__(self, text=''):
+        self.text = text
+aiogram_types.Message = Message
+modules['aiogram'] = aiogram
+modules['aiogram.types'] = aiogram_types
+
+sys.modules.update(modules)
---
+++ tests/test_utils.py
@@ -0,0 +1,62 @@
+import unittest
+
+from services.profticket.profticket_api import ProfticketsInfo
+from services.profticket import utils as pt_utils
+from telegram import tg_utils
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_pluralize(self):
+        self.assertEqual(pt_utils.pluralize('word', 2), 'word_2')
+
+    def test_generate_buy_link(self):
+        api = ProfticketsInfo('42')
+        link = api._generate_buy_link('e1', 's1')
+        exp = (
+            f'{api.CUSTOMER_BUY_URL}42/shows/s1?eventsIds%5B%5D=e1&language=ru-RU'
+        )
+        self.assertEqual(link, exp)
+
+    def test_create_url_without_date(self):
+        api = ProfticketsInfo('42')
+        with self.assertRaises(ValueError):
+            api._create_url(1)
+
+    def test_create_url_with_date(self):
+        api = ProfticketsInfo('42')
+        api.set_date(5, 2024)
+        url = api._create_url(2)
+        exp = (
+            "https://widget.profticket.ru/api/event/list/?company_id=42"
+            "&type=events&page=2&period_id=4&hall_id=&date=2024.5&name=&language=ru-RU"
+        )
+        self.assertEqual(url, exp)
+
+    def test_get_headers(self):
+        api = ProfticketsInfo('42')
+        api.user_agent_provider.get_random_user_agent = lambda: 'UA'
+        headers = api._get_headers()
+        self.assertEqual(headers['User-Agent'], 'UA')
+
+    def test_split_message_by_separator(self):
+        text = (
+            'a\n------------------------\nb\n'
+            '------------------------\nc'
+        )
+        parts = tg_utils.split_message_by_separator(text, max_length=40)
+        self.assertEqual(len(parts), 3)
+        self.assertTrue(parts[0].endswith('------------------------'))
+
+
+class CheckTextTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_check_text(self):
+        msg = tg_utils.Message('Тест Тестович!!!')
+        self.assertEqual(await tg_utils.check_text(msg), 'тест тестович')
+
+    async def test_check_text_invalid(self):
+        msg = tg_utils.Message('invalid')
+        self.assertIsNone(await tg_utils.check_text(msg))
+
+
+if __name__ == '__main__':
+    unittest.main()
